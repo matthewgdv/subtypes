d26cb7033b494373fa05507205fa0d688101bf38
matthewgdv <matthewgdv@gmail.com>
2019-07-11 15:58:56 +0100

    Rework repr() and add Enum.is_enum() and Enum.NotAMemberError()

--- subtypes/enum.py
+++ subtypes/enum.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
 
 from typing import Any
+import enum
 
 import aenum
 
 
 class EnumMeta(aenum.EnumMeta):
     def __repr__(cls) -> str:
-        return f"{cls.__name__}({', '.join([repr(member) for member in cls])})"
+        return f"{cls.__name__}[{', '.join([f'{member.name}={repr(member.value)}' for member in cls])}]"
 
     def __str__(cls) -> str:
-        return f"{', '.join([member.value for member in cls])}"
+        return f"{', '.join([repr(member.value) for member in cls])}"
 
     def __getattribute__(cls, name: str) -> Any:
         value = super().__getattribute__(name)
@@ -23,10 +24,19 @@ class EnumMeta(aenum.EnumMeta):
     def extend_enum(cls, name: str, value: Any) -> EnumMeta:
         aenum.extend_enum(cls, name, value)
 
+    def is_enum(cls, candidate: Any) -> bool:
+        try:
+            return issubclass(candidate, enum.Enum)
+        except TypeError:
+            return False
+
+    def NotAMemberError(cls, value: Any) -> ValueError:
+        return ValueError(f"Invalid {cls.__name__} '{value}', must be one of {cls}.")
+
 
 class Enum(aenum.Enum, metaclass=EnumMeta):
     def __repr__(self) -> str:
-        return f"[{self.name}: {repr(self.value)}]"
+        return f"{type(self).__name__}(name={self.name}, value={repr(self.value)})"
 
     def __str__(self) -> str:
         return str(self.value)
