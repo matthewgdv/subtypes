d4b7307990df35661ca9b1215e2b14061ae65c5d
matthewgdv <matthewgdv@gmail.com>
2019-11-20 12:19:35 +0000

    Add Http subclass of requests.Session

--- subtypes/__init__.py
+++ subtypes/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
     "Enum", "ValueEnum", "AutoEnum", "ValueAutoEnum",
     "Markup",
+    "Http",
     "Singleton",
     "NameSpace", "NameSpaceDict",
     "Str",
@@ -14,6 +15,7 @@ __all__ = [
 
 from .enum import Enum, ValueEnum, AutoEnum, ValueAutoEnum
 from .markup import Markup
+from .http import Http
 from .singleton import Singleton
 from .namespace import NameSpace
 from .str import Str
---
+++ subtypes/http.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any
+
+import requests
+
+
+class Http(requests.Session):
+    def __init__(self, base_url: str) -> None:
+        super().__init__()
+        self.base_url = base_url
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(base_url={repr(self.base_url)}, auth={repr(self.auth)}, headers={repr(self.headers)})"
+
+    def request(self, method: str, url: str, *args: Any, **kwargs: Any) -> Any:
+        return super().request(method=method, url=f"{self.base_url.strip('/')}/{url.strip('/')}", *args, **kwargs)
